8c682e6db2426def37159587ff95998d65cb1623
maadzjaa <magda.g2112@gmail.com>
2023-12-19 10:50:06 +0100

    refactor: small refactor

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,13 @@
 import './style.scss';
 
-const groceries = [
+interface Grocery {
+	text: string;
+	quantity?: number;
+	completed: boolean;
+	id: number;
+}
+
+const groceries: Array<Grocery> = [
 	{
 		text: 'carrot',
 		quantity: 2,
@@ -11,16 +18,41 @@ const groceries = [
 	{ text: 'banana', quantity: 8, completed: false, id: 3 },
 	{ text: 'cookies', quantity: undefined, completed: false, id: 4 },
 ];
+const groceryList = document.querySelector('.list');
+const button = document.querySelector('.add-button');
+const input = document.querySelector('.text-input') as HTMLInputElement | null;
 
 // wyswietl elementy listy w ul
+renderGroceries();
 function renderGroceries() {
-	const groceryList = document.querySelector('.list');
+	if (!groceryList) {
+		return;
+	}
 
+	groceryList.innerHTML = '';
 	groceries.forEach((grocery) => {
 		const groceryLi = document.createElement('li');
+
 		groceryLi.textContent = grocery.text;
 
 		groceryList?.appendChild(groceryLi);
 	});
 }
-renderGroceries();
+
+button?.addEventListener('click', () => {
+	if (!input?.value.trim()) {
+		input!.value = '';
+		return;
+	}
+
+	const newGrocery = {
+		text: input.value,
+		quantity: 2,
+		completed: false,
+		id: groceries.length + 1,
+	};
+
+	groceries.push(newGrocery);
+	input.value = '';
+	renderGroceries();
+});
